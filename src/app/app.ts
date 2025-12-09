0a0fd0b0fe40e8aa2951b237120847c76e4ebd81
Agustin Rubi Hernandez <agusrubher@gmail.com>
2025-12-09 09:05:16 +0100

    Creación de interfaz para añadir y modificar Tareas (Aun pendiente de hacerla útil)

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Task } from './models/task';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +9,26 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('taskboard');
+  private _taskList: Task[] = [];
+  public get taskList(): Task[] {
+    return this._taskList;
+  }
+  public set taskList(value: Task[]) {
+    this._taskList = value;
+  }
+
+  constructor() {
+    if (localStorage.getItem("lista") == null || localStorage.getItem("lista") == "[]") {
+      console.log("No existe almacenamiento previo en el navegador y se procederá a crearlo ahora por primera vez");
+      this.taskList = [{ userId: 1, id: 0, title: "Tarea 1", description: "Esta es la tarea 1", completed: false }];
+      localStorage.setItem("lista", JSON.stringify(this.taskList));
+    } else {
+      console.log("Si existe almacenamiento previo en el navegador y se procederá a cargarlo");
+      if (typeof (localStorage.getItem("lista")) == 'string') {
+        let variableString: any = localStorage.getItem("lista");
+        this.taskList = JSON.parse(variableString);
+      }
+    }
+    console.log("Contenido de la lista de tareas: " + localStorage.getItem("lista"));
+  }
 }
